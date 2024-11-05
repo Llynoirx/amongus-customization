deda46887f679ba156961f1dd5db2bf63fe4fe80
llynoirx <kathyho04@gmail.com>
2024-11-05 00:25:36 -0500

    fix homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-     <div className="sidebar"><Sidebar/></div>
-     Hello
-    </>
+     <div className="app-container">
+       <div className="sidebar"><Sidebar/></div>
+       <h1>Welcome to the Among-Us Crewmate Customization</h1>
+       <p>Here, you will be creating a team of crewmates before sending them off to their mission</p>
+    </div>
   )
 }
 
